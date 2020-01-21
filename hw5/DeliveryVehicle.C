e29ed56295f32e3e30eab92f55addcff7172f875
Yavgenia Shteynman <jenny075@campus.technion.ac.il>
2020-01-21 23:51:53 +0200

    delivery vehcle inprogress

--- hw5/DeliveryVehicle.C
+++ hw5/DeliveryVehicle.C
@@ -9,6 +9,45 @@ DeliveryVehicle::DeliveryVehicle(const char* ID, Quality quality) {
 	queue<*Parcel> parcels_(MAX_PARCELS);
 
 }
+char* DeliveryVehicle::getID() const {
+	return ID_;
+}
+
+Quality DeliveryVehicle::getQuality() const {
+	return quality_;
+}
+
+int DeliveryVehicle::getCount() const {
+	return count_;
+}
+
+int DeliveryVehicle::getStopStation() const {
+	return stopStation_;
+}
+
+int DeliveryVehicle::setStopStation(int stopping_at)  {
+	stopStation_= stopping_at;
+}
+
+bool DeliveryVehicle::addParcel(Parcel* parcel) {
+	int i = 0;
+	if (this.getCount() >= MAX_PARCELS)
+		return false;
+	else {
+		for (i = 0; i < MAX_PARCELS; i++) {
+			if (parcel == parcels_[i]) {
+				parcel->~Parcel();
+				return false;
+			}
+		}
+		this.parcels_.push_back(parcel);
+		this.count_++;
+		return true
+	}
+		
+
+
+}
 
 
 
